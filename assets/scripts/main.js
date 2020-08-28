e14d376a9bb9d94abcdbeb87bd1bfde25e9bd476
K. Marie Brown <k.mariebrown@outlook.com>
2020-08-28 11:14:07 -0500

    📦 NEW: Add main JavaScript file.

---
+++ assets/scripts/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Test for main JavaScript file.
+console.log('This sentence is a test for my main JS file.');
+
+// Welcome Alert.
+window.alert("Welcome! Let's Celebrate Birthdays!");
+
+// Add today's date to the page.
+// var date = new Date();
+// document.write('Today is ' + date);
+
+// Prompts the question, "Is today your birthday".
+function welcomeButton() {
+	var txt;
+	var birthdate = prompt('Is today your birthday? (Type "Yes" or "No")');
+	if (birthdate === 'Yes') {
+		txt = 'Happy Birthday to You!';
+	} else if (birthdate === 'No') {
+		txt = 'A Very Merry Unbirthday to You (yes, you)!';
+	} else {
+		txt = 'You must enter either "Yes" or "No" exactly as you see it here.';
+	}
+	document.getElementById('birthdayWishesButton').innerHTML = txt;
+}
