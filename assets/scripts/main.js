8057e2c58c3ac56f7ea76439b5dc90b117c41669
K. Marie Brown <k.mariebrown@outlook.com>
2020-08-28 17:27:49 -0500

    👌 IMPROVE: Update text.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,12 +3,12 @@
 // Test main JavaScript file with numerical variable and mathematical operator.
 var a = 12;
 var b = 30;
-console.log(a * b);
+console.log('12 * 30 is equal to ' + a * b + '.');
 
 // Welcome Alert.
 window.alert("Welcome! Let's Celebrate Birthdays!");
 
-// Prompts the question, "Is today your birthday".
+// Prompts the question, "Is today your birthday" and adds response to index file.
 function welcomeButton() {
 	var txt;
 	var birthdate = prompt('Is today your birthday? (Type "Yes" or "No")');
@@ -17,7 +17,7 @@ function welcomeButton() {
 	} else if (birthdate === 'No') {
 		txt = 'A Very Merry Unbirthday to You (yes, you)!';
 	} else {
-		txt = 'You must enter either "Yes" or "No" exactly as you see it here.';
+		txt = 'Enter "Yes" or "No" exactly as you see here. Please try again.';
 	}
 	document.getElementById('birthdayWishes').innerHTML = txt;
 }
